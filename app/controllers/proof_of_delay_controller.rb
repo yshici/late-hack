312a37756ad7ff3ba055d5078da31bc40648dffd
yshici <lo.ol.000.yt@gmail.com>
2020-10-24 23:51:05 +0900

    Add: proof_of_delayコントローラー作成

---
+++ app/controllers/proof_of_delay_controller.rb
@@ -0,0 +1,10 @@
+class ProofOfDelayController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :proof_of_delay, only: %i[new create show]
 end
