e26b20faed33e0867a7e89d6f992d2a053ae481a
yshici <lo.ol.000.yt@gmail.com>
2020-10-24 23:51:44 +0900

    Add: delay_infoモデル作成

---
+++ app/models/delay_info.rb
@@ -0,0 +1,10 @@
+class DelayInfo
+  include ActiveModel::Model
+
+  attr_accessor :name, :destination, :current_location, :meeting_time
+
+  validates :name, presence: true
+  validates :destination, presence: true
+  validates :current_location, presence: true
+  validates :meeting_time, present: true
+  end
