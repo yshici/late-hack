ea69f8d5c57c8b93835249a38cd7b77d617217b5
yshici <lo.ol.000.yt@gmail.com>
2020-11-06 08:05:23 +0900

    Add: 列車遅延情報導入

--- app/controllers/proof_of_delays_controller.rb
+++ app/controllers/proof_of_delays_controller.rb
@@ -5,52 +5,47 @@ class ProofOfDelaysController < ApplicationController
   end
 
   def create
-    @delay_info = DelayInfo.new(schedule_params)
-
-
-  end
-
-  def show
-    # require 'uri'
-    # require 'net/http'
-    # require 'openssl'
-
-    # url = URI("https://weather2020-weather-v1.p.rapidapi.com/zip/e8ecee8ff60c478f8a36280fea0524fe/1920073")
-
-    # http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    # request = Net::HTTP::Get.new(url)
-    # request["x-rapidapi-host"] = 'weather2020-weather-v1.p.rapidapi.com'
-    # request["x-rapidapi-key"] = '1812cc7b94mshf09ff39441c8af9p1ff243jsn97878fc23e0a'
-
-    # response = http.request(request)
-    # puts response.read_body
-
+    delay_info = DelayInfo.new(schedule_params)
+    session[:delay_info] = delay_info
+    lat, lng = delay_info.lat_lng.delete("()").split(/,/)
 
     open_weather_api_key = Rails.application.credentials.api_key[:open_weather]
-    # base_url = "http://api.openweathermap.org/data/2.5/forecast"
-    # binding.pry
-    base_url = "https://api.openweathermap.org/data/2.5/onecall?lat=35.6581&lon=139.7017&exclude=hourly,daily&appid=#{ open_weather_api_key }"
+    open_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{ lat.to_f }&lon=#{ lng.to_f }&exclude=hourly,daily&units=metric&lang=ja&appid=#{ open_weather_api_key }"
 
     require "json"
     require "open-uri"
 
-    # response = open(base_url + "?q=Akashi-shi,jp&APPID=#{api_key}")
-    response = open(base_url)
-    # binding.pry
-    # result_weather = JSON.pretty_generate(JSON.parse(response.read))
+    response = open(open_weather_url)
     result_weather = JSON.parse(response.read)
     puts result_weather
+    session[:weather] = result_weather["current"]["weather"][0]["description"]
 
-    # binding.pry
+    train_delay_url = "https://tetsudo.rti-giken.jp/free/delay.json"
+
+    require "json"
+    require "open-uri"
+
+    response = open(train_delay_url)
+    train_delay = JSON.parse(response.read)
+    puts train_delay
+    name_train_delay = []
+    train_delay.map do |t|
+      name_train_delay << t["name"]
+    end
+    session[:name_train_delay] = name_train_delay
+    redirect_to proof_of_delays_path
+  end
+
+  def show
+    @delay_info = session[:delay_info]
+    @weather = session[:weather]
+    @name_train_delay = session[:name_train_delay]
   end
 
 
 private
 
   def schedule_params
-    params.require(:delay_info).permit(:name, :destination, :longitude_latitude, :current_location, :meeting_time)
+    params.require(:delay_info).permit(:name, :destination, :lat_lng, :current_location, :meeting_time)
   end
 end
