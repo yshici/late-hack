6468da7b3cf8c7ac013f40373d9e96c688d6820f
yshici <lo.ol.000.yt@gmail.com>
2020-11-08 19:34:59 +0900

    Add: ルーティング設定、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  resources :shedules
   root 'home#index'
 end
