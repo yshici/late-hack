c7693444daf8f94987c89aab3498a33ea55e7e40
yshici <lo.ol.000.yt@gmail.com>
2020-11-13 15:47:35 +0900

    Add: NHK番組表API追加

--- app/controllers/proof_of_delays_controller.rb
+++ app/controllers/proof_of_delays_controller.rb
@@ -10,30 +10,46 @@ class ProofOfDelaysController < ApplicationController
     session[:delay_info] = delay_info
     lat, lng = delay_info.destination_lat_lng.delete("()").split(/,/)
 
+    # open weather api取得
     open_weather_api_key = Rails.application.credentials.api_key[:open_weather]
     open_weather_url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{ lat.to_f }&lon=#{ lng.to_f }&exclude=hourly,daily&units=metric&lang=ja&appid=#{ open_weather_api_key }"
 
     require "json"
     require "open-uri"
 
-    response = open(open_weather_url)
-    result_weather = JSON.parse(response.read)
+    response_weather = open(open_weather_url)
+    result_weather = JSON.parse(response_weather.read)
     puts result_weather
     session[:weather] = result_weather["current"]["weather"][0]["description"]
 
+
+    # 鉄道遅延情報 api取得
     train_delay_url = "https://tetsudo.rti-giken.jp/free/delay.json"
 
     require "json"
     require "open-uri"
 
-    response = open(train_delay_url)
-    train_delay = JSON.parse(response.read)
+    response_train = open(train_delay_url)
+    train_delay = JSON.parse(response_train.read)
     puts train_delay
     name_train_delay = []
     train_delay.map do |t|
       name_train_delay << t["name"]
     end
     session[:name_train_delay] = name_train_delay
+
+    # NHK番組表 api取得
+    nhk_program_api_key = Rails.application.credentials.api_key[:nhk_program]
+    nhk_program_url = "https://api.nhk.or.jp/v2/pg/now/130/g1.json?key=#{nhk_program_api_key}"
+
+    require "json"
+    require "open-uri"
+
+    response_nhk = open(nhk_program_url)
+    nhk_program = JSON.parse(response_nhk.read)
+    puts nhk_program
+
+
     redirect_to proof_of_delays_path
   end
 
