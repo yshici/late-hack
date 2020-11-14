5e2256de52e116c0749cbb5831efc7d8bf8063d9
yshici <lo.ol.000.yt@gmail.com>
2020-11-15 08:36:52 +0900

    Add: 日時入力で過去の日付を入力不可に設定

--- app/javascript/timepicker.js
+++ app/javascript/timepicker.js
@@ -1,9 +1,12 @@
 // 予定入力フォームのカレンダー部分
+
 $(function () {
+  var now = new Date();
   $('#datetimepicker13').datetimepicker({
+    minDate: now.setHours(now.getHours() - 1),  //i時間前まで選択可能
     format: 'YYYY-MM-DD HH:mm',
     inline: true,
     sideBySide: true,
-    stepping: 60
+    stepping: 60,
   });
 });
