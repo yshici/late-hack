3ad024392486234f9d4e1923a0e60f76854744dc
yshici <lo.ol.000.yt@gmail.com>
2020-11-16 18:53:15 +0900

    Fix: rakeタスク修正

--- app/mailers/delay_info_mailer.rb
+++ app/mailers/delay_info_mailer.rb
@@ -1,4 +1,10 @@
 class DelayInfoMailer < ApplicationMailer
-    @schedule = schedule
-    mail(to: @schedule.user.email, subject: '遅刻しても焦らないで')
+  def delay_info
+    Schedule.find_each do |schedule|
+      if schedule.meeting_time > 1.hours.ago && schedule.meeting_time < 1.hours.since
+        @schedule = schedule
+        mail(to: @schedule.user.email, subject: '遅刻しても焦らないで')
+      end
+    end
+  end
 end
