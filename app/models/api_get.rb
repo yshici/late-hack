0dc12786ffb8e86c3a847cb3d819c9a175ae12b3
yshici <lo.ol.000.yt@gmail.com>
2020-11-25 18:29:43 +0900

    Add: 位置情報使用APIのエラー補足

--- app/models/api_get.rb
+++ app/models/api_get.rb
@@ -45,26 +45,34 @@ class ApiGet
     # ぐるなび api取得
     restaurant_api_key = Rails.application.credentials.api_key[:restaurant_gurunavi]
     restaurant_url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ restaurant_api_key }&latitude=#{ lat }&longitude=#{ lng }"
-    response_restaurant = open(restaurant_url)
-    restaurant_gurunavi = JSON.parse(response_restaurant.read)
-    count = restaurant_gurunavi["rest"].length > 3 ? 3 : restaurant_gurunavi["rest"].length
-    get_restaurant = []
-    restaurant_gurunavi["rest"].first(count).each do |restaurant|
-      get_restaurant << { name: restaurant["name"], category: restaurant["category"], url: restaurant["url"], image_url: restaurant["image_url"]["shop_image1"], address: restaurant["address"] }
+    begin
+      response_restaurant = open(restaurant_url)
+      restaurant_gurunavi = JSON.parse(response_restaurant.read)
+      count = restaurant_gurunavi["rest"].length > 3 ? 3 : restaurant_gurunavi["rest"].length
+      get_restaurant = []
+      restaurant_gurunavi["rest"].first(count).each do |restaurant|
+        get_restaurant << { name: restaurant["name"], category: restaurant["category"], url: restaurant["url"], image_url: restaurant["image_url"]["shop_image1"], address: restaurant["address"] }
+      end
+      get[:restaurant] = get_restaurant
+    rescue
+      get[:restaurant] = nil
     end
-    get[:restaurant] = get_restaurant
 
     # 楽天トラベル api取得
     rakuten_hotel_api_key = Rails.application.credentials.api_key[:hotel_rakuten]
     rakuten_hotel_url = "https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&datumType=1&latitude=#{ lat }&longitude=#{ lng }&applicationId=#{ rakuten_hotel_api_key }"
-    response_hotel = open(rakuten_hotel_url)
-    hotel_rakuten = JSON.parse(response_hotel.read)
-    count = hotel_rakuten["hotels"].length > 3 ? 3 : hotel_rakuten["hotels"].length
-    get_hotel = []
-    hotel_rakuten["hotels"].first(count).each do |hotel|
-      get_hotel << { name: hotel["hotel"][0]["hotelBasicInfo"]["hotelName"], url: hotel["hotel"][0]["hotelBasicInfo"]["hotelInformationUrl"], image_url: hotel["hotel"][0]["hotelBasicInfo"]["hotelImageUrl"], address1: hotel["hotel"][0]["hotelBasicInfo"]["address1"], address2: hotel["hotel"][0]["hotelBasicInfo"]["address2"] }
+    begin
+      response_hotel = open(rakuten_hotel_url)
+      hotel_rakuten = JSON.parse(response_hotel.read)
+      count = hotel_rakuten["hotels"].length > 3 ? 3 : hotel_rakuten["hotels"].length
+      get_hotel = []
+      hotel_rakuten["hotels"].first(count).each do |hotel|
+        get_hotel << { name: hotel["hotel"][0]["hotelBasicInfo"]["hotelName"], url: hotel["hotel"][0]["hotelBasicInfo"]["hotelInformationUrl"], image_url: hotel["hotel"][0]["hotelBasicInfo"]["hotelImageUrl"], address1: hotel["hotel"][0]["hotelBasicInfo"]["address1"], address2: hotel["hotel"][0]["hotelBasicInfo"]["address2"] }
+      end
+      get[:hotel] = get_hotel
+    rescue
+      get[:hotel] = nil
     end
-    get[:hotel] = get_hotel
     get
   end
 end
