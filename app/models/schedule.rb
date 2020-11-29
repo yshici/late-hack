9b470a20567998d5bb2e2ee4d027194755668730
yshici <lo.ol.000.yt@gmail.com>
2020-11-30 03:09:56 +0900

    Add: 結果出力処理追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,16 +1,17 @@
 # schema.rb
 # create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-#     t.string "name"
-#     t.datetime "meeting_time"
-#     t.string "destination_name"
-#     t.string "destination_address"
-#     t.float "destination_lat"
-#     t.float "destination_lng"
-#     t.text "description"
-#     t.integer "user_id"
-#     t.datetime "created_at", precision: 6, null: false
-#     t.datetime "updated_at", precision: 6, null: false
-#   end
+#   t.string "name"
+#   t.datetime "meeting_time"
+#   t.string "destination_name"
+#   t.string "destination_address"
+#   t.float "destination_lat"
+#   t.float "destination_lng"
+#   t.text "description"
+#   t.integer "user_id"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.text "result"
+# end
 
 class Schedule < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
@@ -22,6 +23,8 @@ class Schedule < ApplicationRecord
   validates :description, length: { maximum: 1000 }
   validate  :datetime_not_before
 
+  serialize :result, Hash
+
   def datetime_not_before
     errors.add(:meeting_time, "は現在時刻以降を指定してください") if meeting_time.nil? || meeting_time < DateTime.now.ago(1.hours)
   end
@@ -30,5 +33,17 @@ class Schedule < ApplicationRecord
 
   def start_time
     self.meeting_time
+  end
+
+  def get_result
+    get_api_info = ApiGet.new().get_api
+    Schedule.find_each do |schedule|
+      if schedule.meeting_time > 1.hours.ago && schedule.meeting_time < 1.hours.since
+        get_api_info_with_position = ApiGet.new().get_api_with_position(schedule.destination_lat, schedule.destination_lng)
+        schedule.result = get_api_info.merge(get_api_info_with_position)
+        schedule.save!
+        DelayInfoMailer.delay_info(schedule).deliver_now
+      end
+    end
   end
 end
