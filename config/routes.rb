11461ee0ef0c0941d0497a218c21058759a4a52b
yshici <lo.ol.000.yt@gmail.com>
2020-11-30 03:08:56 +0900

    Add: 結果表示ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :schedules
+  resources :schedules, shallow: true do
+    resources :results, only: %i[show]
+  end
   root 'home#index'
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
