2dc432e2db8106c0fdd1561cbeee82639107943d
yshici <lo.ol.000.yt@gmail.com>
2020-12-13 23:05:58 +0900

    Add: ログイン使用の結果表示ページに経路追加

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,8 @@
 class ResultsController < ApplicationController
   def show
     @schedule = current_user.schedules.find(params[:id])
+    # Jsに渡す変数s
+    gon.latlng = [[@schedule.destination_lat,@schedule.destination_lng],[@schedule.start_point_lat,@schedule.start_point_lng]]
+    gon.excuse = ExcuseForLate.new().excuse
   end
 end
