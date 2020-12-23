9d6ba5f182b277afe8694628182e30491f104b0b
yshici <lo.ol.000.yt@gmail.com>
2020-12-23 19:23:03 +0900

    Add: scheduleモデルスペック追加

---
+++ spec/factories/schedules.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :schedule do
+    name { '予定名' }
+    meeting_time { Time.now }
+    destination_name { '渋谷駅' }
+    destination_address { '日本、〒150-0002 東京都渋谷区渋谷２丁目２４' }
+    destination_lat { '35.6580339' }
+    destination_lng { '139.7016358' }
+    start_point_name { '新宿駅' }
+    start_point_address { '日本、〒160-0022 東京都新宿区新宿３丁目３８−１' }
+    start_point_lat { '35.6896067' }
+    start_point_lng { '139.7005713' }
+    association :user
+  end
+end
