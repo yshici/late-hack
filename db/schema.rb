0f722122175cd147986fed43c80b237f6ca83c37
yshici <lo.ol.000.yt@gmail.com>
2020-12-31 17:38:53 +0900

    Add: roleカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_001955) do
+ActiveRecord::Schema.define(version: 2020_12_31_083457) do
 
   create_table "excuse_schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "schedule_id", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_28_001955) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
