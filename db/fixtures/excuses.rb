5ec4e4d250b1f34b01d0f7c1b62497c8e64340cb
yshici <lo.ol.000.yt@gmail.com>
2021-01-02 16:29:02 +0900

    Add: 言い訳seedsデータ追加

---
+++ db/fixtures/excuses.rb
@@ -0,0 +1,44 @@
+Excuse.seed_once(
+  id: 1,
+  content: '財布を落としました!'
+)
+
+Excuse.seed_once(
+  id: 2,
+  content: '携帯を無くしました'
+)
+
+Excuse.seed_once(
+  id: 3,
+  content: 'お腹が痛くなりました'
+)
+
+Excuse.seed_once(
+  id: 4,
+  content: 'お婆ちゃんを助けていました'
+)
+
+Excuse.seed_once(
+  id: 5,
+  content: '拾った財布を交番に届けました'
+)
+
+Excuse.seed_once(
+  id: 6,
+  content: '失神しました'
+)
+
+Excuse.seed_once(
+  id: 7,
+  content: '突き指しました'
+)
+
+Excuse.seed_once(
+  id: 8,
+  content: 'ナンパされました'
+)
+
+Excuse.seed_once(
+  id: 9,
+  content: 'もよおしました'
+)
