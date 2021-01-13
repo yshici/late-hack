18f5054114cda01029d00f0fa8c4d5664ae900a2
yshici <lo.ol.000.yt@gmail.com>
2021-01-13 09:52:43 +0900

    Add: 遅刻時間のバリデーション設定

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -1,5 +1,6 @@
 class Excuse < ApplicationRecord
   validates :content, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :late_time, presence: true
 
   has_many :excuse_schedules
   has_many :schedules, through: :excuse_schedules
