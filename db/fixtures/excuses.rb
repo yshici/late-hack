4d20e92d8c1b47e34c40bfec3de111a4f4c59172
yshici <lo.ol.000.yt@gmail.com>
2021-01-21 10:55:57 +0900

    Add: seedデータに遅刻時間追加

--- db/fixtures/excuses.rb
+++ db/fixtures/excuses.rb
@@ -1,44 +1,53 @@
-Excuse.seed_once(
+Excuse.seed(
   id: 1,
-  content: '財布を落としました!'
+  content: '財布を落としました',
+  late_time: 60
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 2,
-  content: '携帯を無くしました'
+  content: '携帯を無くしました',
+  late_time: 60
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 3,
-  content: 'お腹が痛くなりました'
+  content: 'お腹が痛くなりました',
+  late_time: 30
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 4,
-  content: 'お婆ちゃんを助けていました'
+  content: 'お婆ちゃんを助けていました',
+  late_time: 90
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 5,
-  content: '拾った財布を交番に届けました'
+  content: '拾った財布を交番に届けました',
+  late_time: 30
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 6,
-  content: '失神しました'
+  content: '失神しました',
+  late_time: 120
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 7,
-  content: '突き指しました'
+  content: '突き指しました',
+  late_time: 10
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 8,
-  content: 'ナンパされました'
+  content: 'ナンパされました',
+  late_time: 10
 )
 
-Excuse.seed_once(
+Excuse.seed(
   id: 9,
-  content: 'もよおしました'
+  content: 'もよおしました',
+  late_time: 10
 )
